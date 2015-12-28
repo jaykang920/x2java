41b8a346427ee8f9e92725caf34bde34283ce638
Jae-jun Kang <jaykang920@gmail.com>
2015-12-28 18:25:26 +0900

    xpiler: added Definition.java

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# x2java
-Java 5 port of x2
+x2java
+======
+
+_Under development now_
+
+x2java is the Java 5 port of [x2](https://github.com/jaykang920/x2)
+
+Requirements
+------------
+
+* Java 5 or higher version of runtime to run
+* Maven2 to build
+
+License
+-------
+
+x2java is distributed under [MIT License](http://opensource.org/licenses/MIT).
+See the file LICENSE for details.
---
+++ xpiler/src/main/java/xpiler/Definition.java
@@ -0,0 +1,97 @@
+// Copyright (c) 2016 Jae-jun Kang
+// See the file LICENSE for details.
+
+package xpiler;
+
+import java.util.*;
+
+/** Represents an abstract definition. */
+abstract class Definition {
+    public String name;
+
+    //public abstract void format(Formatter.Context context);
+}
+
+/** Represents a set of constants. */
+class ConstsDef extends Definition {
+    /** Represents a constant definition. */
+    public static class Constant {
+        public String name;
+        public String value;
+
+        public String comment;
+    }
+
+    public String type;
+    public String nativeType;
+
+    public String comment;
+
+    private List<Constant> constants = new Vector<Constant>();
+
+    /*
+    @Override
+    public void format(Formatter.Context context) {
+        context.formatConsts(this);
+    }
+    */
+
+    public List<Constant> getConstants() { return constants; }
+
+    public boolean hasConstants() { return !constants.isEmpty(); }
+}
+
+/** Represents a cell definition. */
+class CellDef extends Definition {
+    /** Represents a cell property. */
+    public static class Property {
+        public int index;
+        public String name;
+        public String type;
+        public String defaultValue;
+        public String nativeName;
+        public String nativeType;
+
+        public String comment;
+    }
+
+    public String base;
+    public String baseClass;
+    public boolean isLocal;
+
+    public String comment;
+
+    private List<Property> properties = new Vector<Property>();
+
+    /*
+    @Override
+    public void format(Formatter.Context context) {
+        context.formatCell(this);
+    }
+    */
+
+    public List<Property> getProperties() { return properties; }
+  
+    public boolean hasProperties() { return !properties.isEmpty(); }
+
+    public boolean isEvent() { return false; }
+}
+
+/** Represents an event definition. */
+class EventDef extends CellDef {
+    public String id;
+
+    @Override
+    public boolean isEvent() { return true; }
+}
+
+/** Represents a reference definition. */
+class Reference {
+    public String target;
+
+    /*
+    public void format(Formatter.Context context) {
+        context.formatReference(this);
+    }
+    */
+}
